9a074d571a441148fa1a97d284cd5bc78d72cff0
Romy Chu III <romy.chu-iii@ocsbstudent.ca>
2019-02-28 11:44:25 -0500

    finished day 17

---
+++ 13.RandomNumGenRChuIII/13.RandomNumGenRChuIII/RandomNumGenForm.Designer.cs
@@ -0,0 +1,90 @@
+﻿namespace _13.RandomNumGenRChuIII
+{
+    partial class frmRandomNumGen
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblTitle = new System.Windows.Forms.Label();
+            this.lblNumber = new System.Windows.Forms.Label();
+            this.btnGenerate = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lblTitle
+            // 
+            this.lblTitle.AutoSize = true;
+            this.lblTitle.Location = new System.Drawing.Point(24, 48);
+            this.lblTitle.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.lblTitle.Name = "lblTitle";
+            this.lblTitle.Size = new System.Drawing.Size(645, 25);
+            this.lblTitle.TabIndex = 0;
+            this.lblTitle.Text = "Click the button to generate a random integer between 1 and 1000";
+            // 
+            // lblNumber
+            // 
+            this.lblNumber.AutoSize = true;
+            this.lblNumber.Location = new System.Drawing.Point(24, 167);
+            this.lblNumber.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.lblNumber.Name = "lblNumber";
+            this.lblNumber.Size = new System.Drawing.Size(182, 25);
+            this.lblNumber.TabIndex = 1;
+            this.lblNumber.Text = "Your number is :  ";
+            // 
+            // btnGenerate
+            // 
+            this.btnGenerate.Location = new System.Drawing.Point(29, 97);
+            this.btnGenerate.Name = "btnGenerate";
+            this.btnGenerate.Size = new System.Drawing.Size(156, 39);
+            this.btnGenerate.TabIndex = 2;
+            this.btnGenerate.Text = "Generate";
+            this.btnGenerate.UseVisualStyleBackColor = true;
+            this.btnGenerate.Click += new System.EventHandler(this.btnGenerate_Click);
+            // 
+            // frmRandomNumGen
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(679, 239);
+            this.Controls.Add(this.btnGenerate);
+            this.Controls.Add(this.lblNumber);
+            this.Controls.Add(this.lblTitle);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(6);
+            this.Name = "frmRandomNumGen";
+            this.Text = "Random Number Generator By: Romy I. Chu III";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblTitle;
+        private System.Windows.Forms.Label lblNumber;
+        private System.Windows.Forms.Button btnGenerate;
+    }
+}
+
---
+++ 13.RandomNumGenRChuIII/13.RandomNumGenRChuIII/RandomNumGenForm.cs
@@ -0,0 +1,43 @@
+﻿/*
+ * Created by: Romy I. Chu III
+ * Created on: 28-02-2019
+ * Created for: ICS3U Programming
+ * Daily Assignment – Day #17 - Random Number Generator
+ * This program...Generates a random number when a button is clicked
+*/
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _13.RandomNumGenRChuIII
+{
+    public partial class frmRandomNumGen : Form
+    {
+        public frmRandomNumGen()
+        {
+            InitializeComponent();
+        }
+
+        private void btnGenerate_Click(object sender, EventArgs e)
+        {
+            //declares the variables
+            const int numMin = 1;
+            const int numMax = 1000;
+            int numRandom;
+            Random ranNumGen = new Random();
+
+            //Generates the random number
+            numRandom = ranNumGen.Next(numMin, numMax + 1);
+
+            //Changes the label to what the randomly generated number.
+            lblNumber.Text = "Your number is :  " + Convert.ToString(numRandom);
+        }
+    }
+}
