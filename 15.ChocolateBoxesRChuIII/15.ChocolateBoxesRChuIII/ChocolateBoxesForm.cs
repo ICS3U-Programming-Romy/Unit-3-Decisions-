6f6e8da214e3f634a9e4e4350d0c302d2e5b6a28
Romy Chu III <romy.chu-iii@ocsbstudent.ca>
2019-03-08 12:24:28 -0500

    yes

--- 15.ChocolateBoxesRChuIII/15.ChocolateBoxesRChuIII/ChocolateBoxesForm.cs
+++ 15.ChocolateBoxesRChuIII/15.ChocolateBoxesRChuIII/ChocolateBoxesForm.cs
@@ -1,4 +1,12 @@
-﻿using System;
+﻿/*
+ * Created by: Romy I. Chu III
+ * Created on: 09-03-2019
+ * Created for: ICS3U Programming
+ * Daily Assignment – Day #19 - Chocolate Boxes
+ * This program...has nested if else statements.
+*/
+
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -27,7 +35,8 @@ namespace _15.ChocolateBoxesRChuIII
 
             //Determines the rewards
             if (soldBoxes >= 20)
-            {
+            { 
+                //if the student sells 20 or more they get top prize
                 lblReward.Visible = true;
                 lblReward.Text = "You get the Top Prize!!";
             }
@@ -35,11 +44,13 @@ namespace _15.ChocolateBoxesRChuIII
             {
                 if (soldBoxes <= 10)
                 {
+                    //if the student sells 10 or less they get an honorable mention
                     lblReward.Text = "You Get an Honorable Mention!!";
                     lblReward.Visible = true;
                 }
                 else
                 {
+                    //if the student sells 10 or more, but less than 20 they get a small prize
                     lblReward.Text = "You Get a Small Prize!!";
                     lblReward.Visible = true;
                 }
