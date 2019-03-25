9ebfba163e11c465f6e14daf9f1169ae9ad508ec
Romy Chu III <romy.chu-iii@ocsbstudent.ca>
2019-03-25 11:48:02 -0400

    day  22 done

---
+++ 18.PerfectSquareRootsRChuIII/18.PerfectSquareRootsRChuIII/Form1.Designer.cs
@@ -0,0 +1,94 @@
+﻿namespace _18.PerfectSquareRootsRChuIII
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnStart = new System.Windows.Forms.Button();
+            this.lstPerfectSquares = new System.Windows.Forms.ListBox();
+            this.nudInput = new System.Windows.Forms.NumericUpDown();
+            ((System.ComponentModel.ISupportInitialize)(this.nudInput)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnStart
+            // 
+            this.btnStart.Location = new System.Drawing.Point(50, 129);
+            this.btnStart.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(240, 48);
+            this.btnStart.TabIndex = 1;
+            this.btnStart.Text = "Start";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // lstPerfectSquares
+            // 
+            this.lstPerfectSquares.FormattingEnabled = true;
+            this.lstPerfectSquares.ItemHeight = 25;
+            this.lstPerfectSquares.Location = new System.Drawing.Point(24, 212);
+            this.lstPerfectSquares.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
+            this.lstPerfectSquares.Name = "lstPerfectSquares";
+            this.lstPerfectSquares.Size = new System.Drawing.Size(304, 179);
+            this.lstPerfectSquares.TabIndex = 2;
+            // 
+            // nudInput
+            // 
+            this.nudInput.Location = new System.Drawing.Point(78, 74);
+            this.nudInput.Maximum = new decimal(new int[] {
+            5000,
+            0,
+            0,
+            0});
+            this.nudInput.Name = "nudInput";
+            this.nudInput.Size = new System.Drawing.Size(185, 31);
+            this.nudInput.TabIndex = 0;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(364, 456);
+            this.Controls.Add(this.nudInput);
+            this.Controls.Add(this.lstPerfectSquares);
+            this.Controls.Add(this.btnStart);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.nudInput)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.ListBox lstPerfectSquares;
+        private System.Windows.Forms.NumericUpDown nudInput;
+    }
+}
+
