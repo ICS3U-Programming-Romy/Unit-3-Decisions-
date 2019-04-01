1f0dbef01dd7ed7c67bac44274ac4470b4543592
Romy Chu III <romy.chu-iii@ocsbstudent.ca>
2019-04-01 12:54:30 -0400

    Done day 25

---
+++ 21.ControlObjectRChuII/21.ControlObjectRChuII/ControlObjectsForm.Designer.cs
@@ -0,0 +1,74 @@
+﻿namespace _21.ControlObjectRChuII
+{
+    partial class frmControlObjects
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnClick = new System.Windows.Forms.Button();
+            this.lblYes = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnClick
+            // 
+            this.btnClick.Location = new System.Drawing.Point(94, 81);
+            this.btnClick.Name = "btnClick";
+            this.btnClick.Size = new System.Drawing.Size(75, 23);
+            this.btnClick.TabIndex = 0;
+            this.btnClick.Text = "button1";
+            this.btnClick.UseVisualStyleBackColor = true;
+            this.btnClick.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // lblYes
+            // 
+            this.lblYes.AutoSize = true;
+            this.lblYes.Location = new System.Drawing.Point(36, 9);
+            this.lblYes.Name = "lblYes";
+            this.lblYes.Size = new System.Drawing.Size(35, 13);
+            this.lblYes.TabIndex = 1;
+            this.lblYes.Text = "label1";
+            // 
+            // frmControlObjects
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(353, 192);
+            this.Controls.Add(this.lblYes);
+            this.Controls.Add(this.btnClick);
+            this.Name = "frmControlObjects";
+            this.Text = "Control Object Program By Romy I. Chu III";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnClick;
+        private System.Windows.Forms.Label lblYes;
+    }
+}
+
---
+++ 21.ControlObjectRChuII/21.ControlObjectRChuII/ControlObjectsForm.cs
@@ -0,0 +1,44 @@
+﻿/*
+ * Created by: Romy I. Chu III
+ * Created on: 01-04-2019
+ * Created for: ICS3U Programming
+ * Daily Assignment – Day #27 - Control Objects
+ * This program... that changes the properties of the objects that inherit from control.
+*/
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _21.ControlObjectRChuII
+{
+    public partial class frmControlObjects : Form
+    {
+        public frmControlObjects()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            foreach (Control aControlObject in this.Controls)
+            {
+                aControlObject.BackColor = Color.HotPink;
+
+                if (aControlObject.GetType() == typeof(Label))
+                {
+                    aControlObject.BackColor = Color.Blue;
+                    aControlObject.ForeColor = Color.White;
+                    aControlObject.Location = new Point(110, 140);
+                    lblYes.Text = "This has changed";
+                }
+            }
+        }
+    }
+}
