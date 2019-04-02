1a5646e3c4e5ebb69e3fb4f9fef51e400742eb85
Romy Chu III <romy.chu-iii@ocsbstudent.ca>
2019-04-02 12:43:45 -0400

    Done day 26

---
+++ 22.LettersNestedLoopsRChuIII/22.LettersNestedLoopsRChuIII/LettersNestedLoopsForm.Designer.cs
@@ -0,0 +1,72 @@
+﻿namespace _22.LettersNestedLoopsRChuIII
+{
+    partial class frmLettersNestedLoops
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnStart = new System.Windows.Forms.Button();
+            this.lstLetters = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // btnStart
+            // 
+            this.btnStart.Location = new System.Drawing.Point(98, 12);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(75, 23);
+            this.btnStart.TabIndex = 0;
+            this.btnStart.Text = "Start";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // lstLetters
+            // 
+            this.lstLetters.FormattingEnabled = true;
+            this.lstLetters.Location = new System.Drawing.Point(28, 45);
+            this.lstLetters.Name = "lstLetters";
+            this.lstLetters.Size = new System.Drawing.Size(227, 199);
+            this.lstLetters.TabIndex = 1;
+            // 
+            // frmLettersNestedLoops
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.lstLetters);
+            this.Controls.Add(this.btnStart);
+            this.Name = "frmLettersNestedLoops";
+            this.Text = "Letters Nested Loops Program by Romy I. Chu III";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.ListBox lstLetters;
+    }
+}
+
---
+++ 22.LettersNestedLoopsRChuIII/22.LettersNestedLoopsRChuIII/LettersNestedLoopsForm.cs
@@ -0,0 +1,46 @@
+﻿/*
+ * Created by: Romy I. Chu III
+ * Created on: 02-04-2019
+ * Created for: ICS3U Programming
+ * Daily Assignment – Day #28 - Alphabet Nested Loops
+ * This program...displays the alphabet (A - Z) then the alphabet but not capitalized (a - z)
+*/
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _22.LettersNestedLoopsRChuIII
+{
+    public partial class frmLettersNestedLoops : Form
+    {
+        public frmLettersNestedLoops()
+        {
+            InitializeComponent();
+        }
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            //declares the variables
+            int unicodeLetterCaps;
+            int unicodeLetterLowCase;
+
+            //clears the list
+            lstLetters.Items.Clear();
+
+            for (unicodeLetterCaps = 65; unicodeLetterCaps <= 90; unicodeLetterCaps++)
+            {
+                for(unicodeLetterLowCase = 97; unicodeLetterLowCase <= 122; unicodeLetterLowCase++)
+                {
+                    lstLetters.Items.Add(Char.ConvertFromUtf32(unicodeLetterCaps) + " -> " + Char.ConvertFromUtf32(unicodeLetterLowCase));
+                }
+            }
+        }
+    }
+}
